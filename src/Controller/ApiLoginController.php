47fd7cc322da6aeffe14108e52dcee6e17a11bb7
cryptomagnum <jorge_vankov@hotmail.com>
2024-04-29 15:03:28 +0200

    Descargamos el bundle JWT para generar Jason Web Tokens para la autenticación. Configuramos security.yaml y los firewall De momento dejamos la ruta de autenticacion /login conviviendo con api/login/

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -3,16 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\RegistredUser;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class ApiLoginController extends AbstractController
 {
+    private $passwordHasher;
+    private $JWTManager;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher, JWTTokenManagerInterface $JWTManager)
+    {
+        $this->passwordHasher = $passwordHasher;
+        $this->JWTManager = $JWTManager;
+    }
     #[Route('/login', name: 'login', methods: ['POST'])] // Añadir metodo POST si es necesario
-    public function login(#[CurrentUser] ?RegistredUser $user): JsonResponse
+    public function login(#[CurrentUser] ?RegistredUser $user, Request $request): JsonResponse
     {
         /* 
             {
@@ -20,11 +31,14 @@ class ApiLoginController extends AbstractController
                 "password": "MyPassword"
             }
         */
+        $dataUser = json_decode($request->getContent());
         if (null === $user) {
             return new JsonResponse(['message' => 'Credenciales invalidas', 'status' => 401], Response::HTTP_UNAUTHORIZED);
         }
-
-        $token = 'Hay que crear un token pero no se como';
+        if (!$this->passwordHasher->isPasswordValid($user, $dataUser->password)) {
+            return new JsonResponse(['message' => 'Credenciales invalidas', 'status' => 401], Response::HTTP_UNAUTHORIZED);
+        }
+        $token = $this->JWTManager->create($user);
 
         return new JsonResponse(
             [
