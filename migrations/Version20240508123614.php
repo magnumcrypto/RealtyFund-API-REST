0665ff8bff356451477859e62df8618197e986ac
cryptomagnum <jorge_vankov@hotmail.com>
2024-05-08 14:53:06 +0200

    El contenido es exactamente igual que la versión anterior

--- migrations/Version20240508123614.php
+++ migrations/Version20240508123614.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240429133123 extends AbstractMigration
+final class Version20240508123614 extends AbstractMigration
 {
     public function getDescription(): string
     {
